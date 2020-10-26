34220c923b5d64444dc4c3868e86129a648f5723
suresh <sureshsthite@gmail.com>
2020-10-26 11:43:25 +0530

    Demo for bubbling in JavaScript

---
+++ bubbling.js
@@ -0,0 +1,7 @@
+document.getElementById('parent').addEventListener('click', function(){
+    console.log("parent clicked");
+})
+
+document.getElementById('child').addEventListener('click', function(){
+    console.log("child clicked");
+})
